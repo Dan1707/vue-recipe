7e32c7dad77704f2e0d2cf66787a26d3da386a2a
Dan1707 <danuhae@gmail.com>
2024-06-23 21:30:25 +0300

    finished project

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,12 @@ import "./assets/style.css";
 import { createPinia } from "pinia";
 import globalComponents from "./components/";
 import App from "./App.vue";
-import { VueQueryPlugin } from "vue-query";
 import router from "./router/router.ts";
 
 const app = createApp(App);
 const pinia = createPinia();
 
 app.use(router);
-app.use(VueQueryPlugin);
 app.use(globalComponents);
 app.use(pinia);
 app.mount("#app");
